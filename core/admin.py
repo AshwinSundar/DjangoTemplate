e8de2a335552a5037c8e071fe1322a571c54589d
Ashwin Sundar <ashiundar@gmail.com>
2025-02-09 00:00:38 -0700

    fix(admin.py): adds getFields method

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,27 @@
-# from core.models import ModelA, ModelB
+from core.models import ModelA, ModelB
+
 from django.contrib import admin
+
+from django.db import models
+
+from typing import Type
+
+
+def getFields(c: Type[models.Model]): 
+    return [field.name for field in c._meta.get_fields() 
+            if isinstance(field, models.Field) 
+            and not isinstance(field, models.ManyToManyField)]
 
 
 # @admin.register(ModelA)
-# class WheelAdmin(admin.ModelAdmin):
-#     pass
+class WheelAdmin(admin.ModelAdmin):
+    list_display = getFields(ModelA)  # type: ignore
+    list_filter = getFields(ModelA)  # type: ignore
 
 
 # @admin.register(ModelB)
-# class StockAdmin(admin.ModelAdmin):
-#     pass
+class StockAdmin(admin.ModelAdmin):
+    list_display = getFields(ModelB)  # type: ignore
+    list_filter = getFields(ModelB)  # type: ignore
 
 
