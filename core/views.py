729b29d1c145b0057c8699a9380202571dcf8a3d
Ashwin Sundar <ashiundar@gmail.com>
2025-02-09 00:15:51 -0700

    feat(views.py): Adds utilities

--- core/views.py
+++ core/views.py
@@ -1,5 +1,29 @@
+from django.http import HttpResponse, HttpResponseRedirect
+
 from django.shortcuts import render
+
+from django.template import loader
+
+from core.models import ModelA, ModelB
+
+#
+# UTILITIES
+#
+# HTMX Utility
+# Reference - https://stackoverflow.com/questions/65569673/htmx-hx-target-swap-html-vs-full-page-reload
+class HTTPResponseHXRedirect(HttpResponseRedirect):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self["HX-Redirect"] = self["Location"]
+
+    status_code = 200
 
 
 def home(request):
-    return render(request, "home.html")
+    context = {
+            "as": ModelA.objects.get(),
+            "bs": ModelB.objects.get(),
+    }
+
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render(context, request))
